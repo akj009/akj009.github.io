6db6ea4f9dd2f12500dec58b0d89d78a088b5e83
aj009 <develop.abhij03@gmail.com>
2016-07-24 22:42:11 +0530

    Fixed the animated scroll issue. Added images. Modified structure of about div.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(function() {
+    // for animating the scroll.
     $('a').on('click', function(){
-        console.log('scrolling...');
         event.preventDefault();
-        //debugger;
         var hash = $(this).attr('href');
-        $('.mdl-layout__content').animate({scrollTop: $(hash).offset().top}, 800, function(){
+        
+        var toScroll = $('.mdl-layout__content').scrollTop() + $(hash).offset().top - 68;
+        $('.mdl-layout__content').animate({scrollTop: toScroll}, 800, function(){
             window.location.hash = hash;
         });
     });
     
-    var separatorEl = '<hr class="separatorEl">';
-    $('.mdl-card__title').append(separatorEl);
+    // for highligting the current section in navigation.
+    
+    if($('#about').offset()==0){
+        console.log('About Me');
+    }
+    else if($('#portfolio').offset()==0) {
+        console.log('My works');
+    }
+    else if($('#demos').offset()==0) {
+        console.log('Demos');
+    }
+    else {
+        console.log('Contact me');
+    }
 });
