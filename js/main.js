eda03a2a1ac4e8d1e6cc98f94423364b9fa07af7
aj009 <develop.abhij03@gmail.com>
2016-07-26 20:11:53 +0530

    Added scroll tracker on nav tabs.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     // for animating the scroll.
-    $('a').on('click', function(event){
+    $('nav a').on('click', function(event){
         event.preventDefault();
         var hash = $(this).attr('href');
         
@@ -12,16 +12,32 @@ $(function() {
     
     // for highligting the current section in navigation.
     
-    if($('#about').offset()==0){
-        console.log('About Me');
-    }
-    else if($('#portfolio').offset()==0) {
-        console.log('My works');
-    }
-    else if($('#demos').offset()==0) {
-        console.log('Demos');
-    }
-    else {
-        console.log('Contact me');
-    }
+    $('.mdl-layout__content').scroll(function(){
+        var aboutPos = $('#about').offset().top;
+        var portfolioPos = $('#portfolio').offset().top;
+        var demosPos = $('#demos').offset().top;
+        var contactPos = $('#contact').offset().top;
+        
+        if(aboutPos<70 && aboutPos>50) {
+            $('nav a').css({'font-family':'Roboto,Helvetica,Arial,sans-serif'});
+            $('nav a[href="#about"]').css({'font-family':'Lobster'});
+        }
+        
+        if(portfolioPos<70 && portfolioPos>50) {
+            $('nav a').css({'font-family':'Roboto,Helvetica,Arial,sans-serif'});
+            $('nav a[href="#portfolio"]').css({'font-family':'Lobster'});
+        }
+        
+        if(demosPos<70 && demosPos>50) {
+            $('nav a').css({'font-family':'Roboto,Helvetica,Arial,sans-serif'});
+            $('nav a[href="#demos"]').css({'font-family':'Lobster'});
+        }
+        
+        if(contactPos<70 && contactPos>50) {
+            $('nav a').css({'font-family':'Roboto,Helvetica,Arial,sans-serif'});
+            $('nav a[href="#contact"]').css({'font-family':'Lobster'});
+             
+        }
+        
+    })
 });
